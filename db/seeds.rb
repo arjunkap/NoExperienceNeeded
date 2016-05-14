9b2f109a0d7e9765ce9d17e4e6070534c78da5b4
Grace Geddes <grace@gracegeddes.com>
2016-05-14 04:35:56 +0000

    stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,7 @@
 # Job.create(:title => "Software engineer", :description => "This sis aasdfadsfas", :country => "Australia", :city => "Melbourne")
 # Job.create(:title => "Software engineer", :description => "This sis aasdfadsfas", :country => "Australia", :city => "Melbourne")
 # Job.create(:title => "Software engineer", :description => "This sis aasdfadsfas", :country => "Australia", :city => "Melbourne")
+# Job.create(:title => "Life", :description => "good stuff", :country => "Australia", :city => "Perth") 
 
 # u = User.create(:first_name => "Yash", email: "4s56sdsd7a8sdfsd@gmail.com", password: "123456", :city => "Sydney")
 # u.company = Company.new(:title => "Facebook", :abn => "12345678")
@@ -19,10 +20,10 @@
 # u.jobs.create(:title => "IT Staff", :description => "This sis aasdfadsfas", :country => "Australia", :city => "Sydney", sub_industry_id: s.id, :work_type => "FT")
 
 # User.create(:first_name => "Yash", email: "4567a8sdfsd@gmail.com", password: "123456", :city => "Sydney")
-# # u.company = Company.new(:title => "Wunderman", :abn => "12345678")
-# # u.company.save
-# # s = SubIndustry.create(name: "IT")
-# # u.jobs.create(:title => "IT Staff", :description => "This sis aasdfadsfas", :country => "Australia", :city => "Sydney", sub_industry_id: s.id, :work_type => "FT")
+# u.company = Company.new(:title => "Wunderman", :abn => "12345678")
+# u.company.save
+# s = SubIndustry.create(name: "IT")
+# u.jobs.create(:title => "IT Staff", :description => "This sis aasdfadsfas", :country => "Australia", :city => "Sydney", sub_industry_id: s.id, :work_type => "FT")
 
 
 
@@ -38,16 +39,16 @@
 
 ###############
 #Interviewas
- u = User.create(:first_name => "Yash", email: "12sssdsd5asd04@gmail.com", password: "123456", :city => "Sydney")
- u.job_seeker = JobSeeker.create(mobile_number: "0416021705")
- u = JobSeeker.first
-c = Company.first
-first = u.interview_reviews.create(title: "Awesome", company_id: c.id, company_name: c.title, description: "The interview was great", position: "Software developer", experience: "Graduate level", offer:  true, difficulty: 5, duration: 1)
-second = u.interview_reviews.create(title: "Bad", company_id: c.id, company_name: c.title, description: "The interview was great", position: "Software engineer", experience: "2 years experience", offer:  true, difficulty: 4, duration: 1)
-third = u.interview_reviews.create(title: "Awesome", company_id: c.id, company_name: c.title, description: "The interview was great", position: "Technological analyst", experience: "Graduate level", offer:  true, difficulty: 5, duration: 1)
-fourth = u.interview_reviews.create(title: "asdfasd", company_id: c.id, company_name: c.title, description: "The interview was great", position: "Software developer", experience: "Graduate level", offer:  true, difficulty: 10, duration: 1)
-first = u.interview_reviews.create(title: "Awesome", company_id: 1, company_name: "aCompany", description: "The interview was great", position: "Software developer", experience: "Graduate level", offer:  true, difficulty: 5, duration: 1)
-second = u.interview_reviews.create(title: "Awesome", company_id: 2, company_name: "bCompany", description: "The interview was great", position: "Software developer", experience: "Graduate level", offer:  true, difficulty: 5, duration: 1)
+ # u = User.create(:first_name => "Yash", email: "12sssdsd5asd04@gmail.com", password: "123456", :city => "Sydney")
+ # u.job_seeker = JobSeeker.create(mobile_number: "0416021705")
+ # u = JobSeeker.first
+# c = Company.first
+# first = u.interview_reviews.create(title: "Awesome", company_id: c.id, company_name: c.title, description: "The interview was great", position: "Software developer", experience: "Graduate level", offer:  true, difficulty: 5, duration: 1)
+# second = u.interview_reviews.create(title: "Bad", company_id: c.id, company_name: c.title, description: "The interview was great", position: "Software engineer", experience: "2 years experience", offer:  true, difficulty: 4, duration: 1)
+# third = u.interview_reviews.create(title: "Awesome", company_id: c.id, company_name: c.title, description: "The interview was great", position: "Technological analyst", experience: "Graduate level", offer:  true, difficulty: 5, duration: 1)
+# fourth = u.interview_reviews.create(title: "asdfasd", company_id: c.id, company_name: c.title, description: "The interview was great", position: "Software developer", experience: "Graduate level", offer:  true, difficulty: 10, duration: 1)
+# first = u.interview_reviews.create(title: "Awesome", company_id: 1, company_name: "aCompany", description: "The interview was great", position: "Software developer", experience: "Graduate level", offer:  true, difficulty: 5, duration: 1)
+# second = u.interview_reviews.create(title: "Awesome", company_id: 2, company_name: "bCompany", description: "The interview was great", position: "Software developer", experience: "Graduate level", offer:  true, difficulty: 5, duration: 1)
 
 # q = Question.create(:question => "How was your interview expereience ?")
 # iq.question_id = q.id
@@ -60,26 +61,26 @@ second = u.interview_reviews.create(title: "Awesome", company_id: 2, company_nam
 # ###################
 
 # #Review
-u = User.create(:first_name => "Yash", email: "yash.narwal@gmail.com", password: "123456", :city => "Sydney")
-u.company = Company.create(abn: "12345679", title: "Google")
-u.company.save
+# u = User.create(:first_name => "Yash", email: "yash.narwal@gmail.com", password: "123456", :city => "Sydney")
+# u.company = Company.create(abn: "12345679", title: "Google")
+# u.company.save
 
-c = Company.last
+# c = Company.last
 
-c.reviews.create(title: "This is a review", isAnonymous: true, description: "This is a verry good interviews", company_name: "Google")
-
-
-c.reviews.create(title: "This is a review", isAnonymous: true, description: "This is a verry good interviews", company_name: "Google")
+# c.reviews.create(title: "This is a review", isAnonymous: true, description: "This is a verry good interviews", company_name: "Google")
 
 
-c.reviews.create(title: "This is a review", isAnonymous: true, description: "This is a verry good interviews", company_name: "Google")
+# c.reviews.create(title: "This is a review", isAnonymous: true, description: "This is a verry good interviews", company_name: "Google")
+
+
+# c.reviews.create(title: "This is a review", isAnonymous: true, description: "This is a verry good interviews", company_name: "Google")
 
 
 
-c.reviews.create(title: "This is a review", isAnonymous: true, description: "This is a verry good interviews", company_name: "Microsoft")
+# c.reviews.create(title: "This is a review", isAnonymous: true, description: "This is a verry good interviews", company_name: "Microsoft")
 
 
-c.reviews.create(title: "This is a review", isAnonymous: true, description: "This is a verry good interviews", company_name: "Mocrosoft")
+# c.reviews.create(title: "This is a review", isAnonymous: true, description: "This is a verry good interviews", company_name: "Mocrosoft")
 
 
 
